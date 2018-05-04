49b8bae9a0e8c5c2b9b2d5ac540f465bf9a7db2c
Mohamed Kassim <m_kassim90@yahoo.com>
2018-05-04 16:14:01 +0200

    reading encoders and speeds

--- mobile_robot/mobile_robot.ino
+++ mobile_robot/mobile_robot.ino
@@ -4,6 +4,10 @@ int lSet1 = 6;
 int lSet2 = 7;
 int rEnable = 3;
 int lEnable = 11;
+int rEncoder = 12;
+int lEncoder = 13;
+volatile unsigned int rMotorTicks = 0;
+volatile unsigned int lMotorTicks = 0;
 
 //initializing variables
 int rMotorSpeed = 0, lMotorSpeed = 0;
@@ -16,30 +20,50 @@ void setup() {
   pinMode(lSet2, OUTPUT);
   pinMode(rEnable, OUTPUT);
   pinMode(lEnable, OUTPUT);
+  pinMode(rEncoder, INPUT);
+  digitalWrite(rEncoder, HIGH);       // turn on pull-up resistor
+  pinMode(lEncoder, INPUT);
+  digitalWrite(lEncoder, HIGH); 
+  attachInterrupt(digitalPinToInterrupt(rEncoder), doRightEncoder, HIGH); 
+  attachInterrupt(digitalPinToInterrupt(lEncoder), doLeftEncoder, HIGH);
   Serial.begin(9600);
 
 }
 
-void rightWheel(rmos){
-  digitalWrite(rset1, LOW);
-  digitalWrite(rset2, HIGH);
+void rightWheel(int rmos){
+  digitalWrite(rSet1, LOW);
+  digitalWrite(rSet2, HIGH);
   analogeWrite(rEnable, rmos);
 }
 
-void leftWheel(lmos){
-  digitalWrite(lset1, LOW);
-  digitalWrite(lset2, HIGH);
+void leftWheel(int lmos){
+  digitalWrite(lSet1, LOW);
+  digitalWrite(lSet2, HIGH);
   analogeWrite(lEnable, lmos);
+}
+void doRightEncoder(){
+  rMotorTicks++;
+}
+void doLeftEncoder(){
+  lMotorTicks++;
 }
 void loop() {
   // put your main code here, to run repeatedly:
   //suppose we run in forward motion 
-  rMotorSpeed = 70;
-  lMotorSpeed = 70;
-  leftWheel(rMotorSpeed);
-  rightWheel(lMotorSpeed);
-  Serial.print(leftWheel)
-  Serial.print(rightWheel)
+  rMotorSpeed = rMotorTicks / 8;
+  lMotorSpeed = lMotorTicks / 8;
+  for (int i = 0 ; i<= 255 ; i+=10){
+  leftWheel(i);
+  Serial.printIn(" V ,Left  Mapped speed");
+  Serial.print(i);
+  Serial.print(lMotorSpeed);
+  rightWheel(i);
+  Serial.printIn(" V ,Right Mapped speed");
+  Serial.print(i);
+  Serial.print(rMotorSpeed);
+  }
+
+
   Serial.println("");
 
 }
